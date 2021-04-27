2775fc9bd3d61910ee470dfca66d9c3a348ada7e
ACB <78452262+adambedingfield@users.noreply.github.com>
2021-04-27 13:33:54 -0700

    user response pushed to array for html mapping

--- src/template.js
+++ src/template.js
@@ -4,28 +4,25 @@ const Manager = require("../lib/Manager");
 const Engineer = require("../lib/Engineer");
 const { writeHTML } = require("../utils/generateHTML");
 
-const createManager = (Manager) => {
-    return writeHTML(`${Manager.name},
-${Manager.id},
-${Manager.email},
-${Manager.office}
-    `);
+const createManager = Manager => {
+    return `${Manager.getName()},
+    ${Manager.getId()},
+    ${Manager.getEmail()},
+    ${Manager.getOffice()}`;
 };
 
 const createEngineer = (Engineer) => {
-    return writeHTML(`${Engineer.name},
-${Engineer.id},
-${Engineer.email},
-${Engineer.github}
-    `);
+    return `${Engineer.getName()},
+    ${Engineer.getId()},
+    ${Engineer.getEmail()},
+    ${Engineer.getGitHub()}`;
 };
 
 const createIntern = (Intern) => {
-    return writeHTML(`${Intern.name},
-${Intern.id},
-${Intern.email},
-${Intern.school}
-    `);
+    return `${Intern.getName()},
+    ${Intern.getId()},
+    ${Intern.getEmail()},
+    ${Intern.getSchool()}`;
 };
 
 module.exports = { createEngineer, createIntern, createManager };
