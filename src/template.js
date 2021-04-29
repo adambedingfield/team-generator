7d03f4fbc533c3c69d9773b4870bace4815d30d4
ACB <78452262+adambedingfield@users.noreply.github.com>
2021-04-29 03:15:29 -0700

    html/css finished

--- src/template.js
+++ src/template.js
@@ -1,70 +1,93 @@
-const { writeFile } = require("fs");
 const Employee = require("../lib/Employee");
 const Manager = require("../lib/Manager");
 const Engineer = require("../lib/Engineer");
-const { writeHTML } = require("../utils/generateHTML");
+const { writeHTML, copyFile } = require("../utils/generateHTML");
 
 const createManager = employed => {
+    copyFile();
     return writeHTML(`
-    <!DOCTYPE html>
-    <html lang="en">
-  
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Team Sheet</title>
-      <link rel="stylesheet" href="style.css">
-    </head>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Team Sheet</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+</head>
+<header>
+    <h1>My Team</h1>
+</header>
     <body>
+    <section class="Employee-Holder">
+    <div class="row">
         ${employed
           .filter(({ office }) => office)
           .map(({ name, id, email, role, office }) => {
-          return `
-            <section class="my-3" id="about">
-            <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-            <p>${name}</p>
-            <p>${id}</p>
-            <p>${email}</p>
-            <p>${role}</p>
-            <p>${office}</p>
-            </section>
-          `;
-          })
-          .join('')}
+          return `<!-- Manager -->
+              <div class="container col-5 card card-top">
+                  <div class="row">
+                    <h4 class="col-6 Name">${name} </h4>
+                    <h4 class="col-6 Role">${role} &#9999;</h4>
+                  </div>
+                  <div class="info">
+                    <p class="col-12">ID: ${id}</p>
+                    <p class="col-12">Email: <a href = "mailto: ${email}">${email}</a></p>
+                    <p class="col-12">Office: ${office} </p>
+                  </div>
+              </div>
+              `;
+            })
+            .join()}       
+            </div>
+        </section>
+        <section class="Employee-Holder">
+          <div class="row">
         ${employed
-          .filter(({ github }) => github)
-          .map(({ name, id, email, role, github }) => {
-          return `
-            <section class="my-3" id="about">
-            <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-            <p>${name}</p>
-            <p>${id}</p>
-            <p>${email}</p>
-            <p>${role}</p>
-            <p>${github}</p>
-            </section>
-          `;
+            .filter(({ github }) => github)
+            .map(({ name, id, email, role, github }) => {
+            return `<!-- Engineer -->
+                <div class="container col-5 card card-top">
+                    <div class="row">
+                      <h4 class="col-6 Name">${name} </h4>
+                      <h4 class="col-6 Role">${role} &#9881;</h4>
+                    </div>
+                    <div class="info">
+                      <p class="col-12">ID: ${id}</p>
+                      <p class="col-12">Email: <a href = "mailto: ${email}">${email}</a></p>
+                      <p class="col-12">Github: <a href="https://github.com/${github}">${github}</a></p>
+                    </div>
+                </div>
+            `;
           })
           .join('')}
+
         ${employed
           .filter(({ school }) => school)
           .map(({ name, id, email, role, school }) => {
-          return `
-            <section class="my-3" id="about">
-            <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-            <p>${name}</p>
-            <p>${id}</p>
-            <p>${email}</p>
-            <p>${role}</p>
-            <p>${school}</p>
-            </section>
-            `;
+          return `<!-- Intern -->
+              <div class="container col-5 card card-top">
+                  <div class="row">
+                    <h4 class="col-6 Name">${name} </h4>
+                    <h4 class="col-6 Role">${role} &#9749;</h4>
+                  </div>
+                  <div class="info">
+                    <p class="col-12">ID: ${id}</p>
+                    <p class="col-12">Email: <a href = "mailto: ${email}">${email}</a></p>
+                    <p class="col-12">School: ${school}</p>
+                  </div>
+              </div>
+              `;
             })
             .join('')}
-    </body>
-      `);
-    };
+            </div>
+        </section>
+        
+        
+</body>`);
+};
 
 const createEngineer = () => {
     createManager();
--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -15,4 +15,22 @@ const writeHTML = fileContent => {
     });
 };
 
-module.exports = { writeHTML };
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            //if thers an error reject the promise and send error to promise .catch()
+            if (err) {
+                reject(err);
+                // return out of the function to make sure promise doesnt execute
+                return;
+            }
+            // if everything went well, resolve promise and send successful data to the .then()
+            resolve({
+                ok: true,
+                message: 'Stylesheet created!'
+            });
+        });
+    });
+};
+
+module.exports = { writeHTML, copyFile};
