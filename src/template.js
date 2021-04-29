4139b464f25ea52245ecfc00aa0559a8ecb0ff63
ACB <78452262+adambedingfield@users.noreply.github.com>
2021-04-29 02:02:21 -0700

    html being displayed with answers, started test html, added style sheet

--- src/template.js
+++ src/template.js
@@ -4,25 +4,79 @@ const Manager = require("../lib/Manager");
 const Engineer = require("../lib/Engineer");
 const { writeHTML } = require("../utils/generateHTML");
 
-const createManager = Manager => {
-    return `${Manager.getName()},
-    ${Manager.getId()},
-    ${Manager.getEmail()},
-    ${Manager.getOffice()}`;
-};
+const createManager = employed => {
+    return writeHTML(`
+    <!DOCTYPE html>
+    <html lang="en">
+  
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Team Sheet</title>
+      <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        ${employed
+          .filter(({ office }) => office)
+          .map(({ name, id, email, role, office }) => {
+          return `
+            <section class="my-3" id="about">
+            <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+            <p>${name}</p>
+            <p>${id}</p>
+            <p>${email}</p>
+            <p>${role}</p>
+            <p>${office}</p>
+            </section>
+          `;
+          })
+          .join('')}
+        ${employed
+          .filter(({ github }) => github)
+          .map(({ name, id, email, role, github }) => {
+          return `
+            <section class="my-3" id="about">
+            <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+            <p>${name}</p>
+            <p>${id}</p>
+            <p>${email}</p>
+            <p>${role}</p>
+            <p>${github}</p>
+            </section>
+          `;
+          })
+          .join('')}
+        ${employed
+          .filter(({ school }) => school)
+          .map(({ name, id, email, role, school }) => {
+          return `
+            <section class="my-3" id="about">
+            <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+            <p>${name}</p>
+            <p>${id}</p>
+            <p>${email}</p>
+            <p>${role}</p>
+            <p>${school}</p>
+            </section>
+            `;
+            })
+            .join('')}
+    </body>
+      `);
+    };
 
-const createEngineer = (Engineer) => {
-    return `${Engineer.getName()},
-    ${Engineer.getId()},
-    ${Engineer.getEmail()},
-    ${Engineer.getGitHub()}`;
+const createEngineer = () => {
+    createManager();
 };
 
 const createIntern = (Intern) => {
-    return `${Intern.getName()},
-    ${Intern.getId()},
-    ${Intern.getEmail()},
-    ${Intern.getSchool()}`;
+    return writeHTML(`${Intern.name},
+${Intern.id},
+${Intern.email},
+${Intern.school}
+    `);
 };
+
 
 module.exports = { createEngineer, createIntern, createManager };
